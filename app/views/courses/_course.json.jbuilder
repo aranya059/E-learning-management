95e96c4d6deab4ea07c71b3ec2de2a460c0fab82
aranya059 <aranya.riaz@gmail.com>
2022-01-03 21:20:24 +0600

    MySQL database added

--- app/views/courses/_course.json.jbuilder
+++
@@ -1,2 +0,0 @@
-json.extract! course, :id, :course_id, :title, :description, :created_at, :updated_at
-json.url course_url(course, format: :json)
--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_02_152724) do
-
-  create_table "courses", force: :cascade do |t|
-    t.integer "course_id"
-    t.string "title"
-    t.string "description"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
+ActiveRecord::Schema.define(version: 0) do
 
 end
